c385ef1b3aa8978815507a11da5d1aee748e85ff
Kylin <liux1118@qq.com>
2022-09-10 22:13:43 +0800

    feat: 对环境配置进行校验

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 // 环境配置相关
 import { ConfigModule } from '@nestjs/config';
+// 校验环境变量
+import * as Joi from '@hapi/joi';
 // import { User } from './user/entities/user.entity';
 // import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
@@ -17,10 +19,20 @@ import { RoleModule } from './role/role.module';
 @Module({
   imports: [
     /**
-     * 会解析根目录下的.env文件，并将其中的键值对分配给process.env的环境变量合并
+     * 默认情况下会解析根目录下的.env文件，并将其中的键值对分配给process.env的环境变量合并
      * 将合并结果存储在私有结构中，可以通过ConfigService类在任何位置访问该结构
      */
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      // 校验传入的环境变量是否是正确格式
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(), // 必须传
+        DATABASE_PORT: Joi.number().default(5432) // 默认值
+      })
+    }),
+    // ConfigModule.forRoot({
+    //   envFilePath: '.environment', // 指定.environment文件
+    //   ignoreEnvFile: true, // 忽略.env文件
+    // }),
     UserModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
